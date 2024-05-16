3c93f0e38bc9c3df881e868a1b5869fbeea0a59b
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-16 16:13:37 +0900

    Title: 최소공배수 구하기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240516/최소공배수 구하기/find-the-least-common-multiple.py
+++ 240516/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,21 +1,9 @@
 N, M = map(int, input().split())
 
-def measure(cnt):
-    measures = []
-    for i in range(1, cnt+1):
-        if cnt % i == 0:
-            measures.append(i)
-    return measures
 
 def LCM(n, m):
-    measure_list = []
-    result = 1
-    for i in range(len(measure(n))):
-        for j in range(len(measure(m))):
-            if measure(n)[i] == measure(m)[j]:
-                measure_list.append(measure(n)[i])
-    for i in range(len(measure_list)):
-        result = result * measure_list[i]
-    print(result)
+    for i in range(max(n,m), n*m+1):
+        if i % n == 0 and i % m == 0:
+            return i
 
-LCM(N, M)
+print(LCM(N, M))
