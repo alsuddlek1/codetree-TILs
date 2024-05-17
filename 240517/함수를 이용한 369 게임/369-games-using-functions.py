c10e0841368ad926a5a79f7c8121e1bae26cd60d
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-17 15:28:39 +0900

    Title: 함수를 이용한 369 게임, Time: 129ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240517/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240517/함수를 이용한 369 게임/369-games-using-functions.py
@@ -8,21 +8,25 @@ def count_num(c1, c2):
     for i in range(c1, c2+1):
         if i % 10 == 3 or i % 10 == 6 or i % 10 == 9 or i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
             in_cnt.append(i)
+        elif i % 3 == 0:
+            in_cnt.append(i)
     return in_cnt
 
-def three_multiple(c1, c2):
-    multi_cnt = []
-    for i in range(c1, c2+1):
-        if i % 3 == 0:
-            multi_cnt.append(i)
-    return multi_cnt
+print(len(count_num(c1, c2)))
+
+# def three_multiple(c1, c2):
+#     multi_cnt = []
+#     for i in range(c1, c2+1):
+#         if i % 3 == 0:
+#             multi_cnt.append(i)
+#     return multi_cnt
 
 
-cnt_list = []
-for i in count_num(c1, c2):
-    cnt_list.append(i)
-    for j in three_multiple(c1, c2):
-        cnt_list.append(j)
+# cnt_list = []
+# for i in count_num(c1, c2):
+#     cnt_list.append(i)
+#     for j in three_multiple(c1, c2):
+#         cnt_list.append(j)
 
-result = set(cnt_list)
-print(len(result))
+# result = set(cnt_list)
+# print(len(result))
