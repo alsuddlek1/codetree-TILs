eb996f53789acf980d350f6d4f79aa4cccd632a8
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-17 15:14:08 +0900

    Title: 함수를 이용한 369 게임, Time: 1319ms, Memory: 288MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240517/함수를 이용한 369 게임/369-games-using-functions.py
@@ -0,0 +1,28 @@
+## a~b 사이에 3,6,9
+## 숫자가 3의 배수
+
+c1, c2 = map(int, input().split())
+
+def count_num(c1, c2):
+    in_cnt = []
+    for i in range(c1, c2+1):
+        if i % 10 == 3 or i % 10 == 6 or i % 10 == 9 or i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+            in_cnt.append(i)
+    return in_cnt
+
+def three_multiple(c1, c2):
+    multi_cnt = []
+    for i in range(c1, c2+1):
+        if i % 3 == 0:
+            multi_cnt.append(i)
+    return multi_cnt
+
+
+cnt_list = []
+for i in count_num(c1, c2):
+    cnt_list.append(i)
+    for j in three_multiple(c1, c2):
+        cnt_list.append(j)
+
+result = set(cnt_list)
+print(len(result))
