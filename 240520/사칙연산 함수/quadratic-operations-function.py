0f99dbd5bfa60b2c18a7bbee77d3b7e6675803e3
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-20 15:31:32 +0900

    Title: 사칙연산 함수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240520/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,20 @@
+a,o,c = input().split()
+a = int(a)
+c = int(c)
+
+def o_judg(o):
+    if o == "/":
+        return a / c
+    if o == '*':
+        return a * c
+    if o == '+':
+        return a + c
+    if o == '-':
+        return a - c
+    else:
+        return False
+
+if o_judg(o):
+    print(f'{a} {o} {c} = {o_judg(o)}')
+else:
+    print("False")
