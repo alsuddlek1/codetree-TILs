4cbc8dbc7807a8354c7a7723b3dd31bdb74d9114
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-20 15:52:22 +0900

    Title: 함수를 이용한 온전수 판별, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240520/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 240520/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -1,18 +1,36 @@
 a,b = map(int, input().split())
 
-def perfect_num(a,b):
-    result = 0
-    for i in range(a, b+1):
-        judg = 0
-        if i // 2 != 0:
-            judg += 1
-        if i % 10 != 5:
-            judg += 1
-        if i % 3 == 0 and i % 9 != 0:
-            judg += 1
-        
-        if judg == 3:
-            result += 1
-    return result
+def perfect_num(i):
+    if i % 2 == 0:
+        return False
+    if i % 10 == 5:
+        return False
+    if i % 3 == 0 and i % 9 != 0:
+        return False
+    return True
 
-print(perfect_num(a,b))
+result = 0
+for i in range(a, b+1):
+    # print(perfect_num(i), i)
+    if perfect_num(i):
+        result += 1
+
+print(result)
+
+
+
+# def perfect_num(a,b):
+#     result = 0
+#     for i in range(a, b+1):
+#         judg = 0
+#         if i // 2 == 0:
+#             return False
+#         if i % 10 == 5:
+#             return False
+#         if i % 3 != 0 and i % 9 == 0:
+#             return False
+        
+#         result += 1
+#     return result
+
+# print(perfect_num(a,b))
