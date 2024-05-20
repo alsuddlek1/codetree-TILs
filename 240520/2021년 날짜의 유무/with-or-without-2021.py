867be1d0080d00106b693dcec6ae8920749920cb
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-20 16:23:25 +0900

    Title: 2021년 날짜의 유무, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240520/2021년 날짜의 유무/with-or-without-2021.py
+++ 240520/2021년 날짜의 유무/with-or-without-2021.py
@@ -6,9 +6,10 @@ def day(M, D):
         day = 31
         if M == 2:
             day = 28
-        if M % 2 == 0 and M != 2:
+        if M <= 6 and M % 2 == 0 and M != 2:
             day = 30
-        
+        if M > 6 and M % 2 == 1:
+            day = 30
         if D <= day:
             return "Yes"
     return "No"
