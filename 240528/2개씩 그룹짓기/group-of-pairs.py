6d41b73e288334987a2f1ad78b72b25f16767db3
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-05-28 16:09:57 +0900

    Title: 2개씩 그룹짓기, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240528/2개씩 그룹짓기/group-of-pairs.py
+++ 240528/2개씩 그룹짓기/group-of-pairs.py
@@ -1,4 +1,10 @@
 N = int(input())
 data = list(map(int, input().split()))
 data.sort()
-print(data[N]+data[N-1])
+
+max_value = 0
+for i in range(N):
+    if data[i] + data[2*N-1-i] > max_value:
+        max_value = data[i] + data[2*N-1-i]
+
+print(max_value)
