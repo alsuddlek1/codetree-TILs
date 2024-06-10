364868b18d8c5b6e470f4c50c72b035342c438a7
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-06-10 15:14:25 +0900

    Title: 요일 맞추기, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240610/요일 맞추기/guess-day-of-week.py
+++ 240610/요일 맞추기/guess-day-of-week.py
@@ -6,18 +6,36 @@ months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 day = 2
 
-while True:
-    if m1 == m2 and d1 == d2:
-        break
+# 1이 2보다 이후
+if m1 > m2 or (m1 == m2 and d1 > d2):
 
-    d1 -= 1
-    day -= 1
+    while True:
+        if m1 == m2 and d1 == d2:
+            break
 
-    if day == 0:
-        day = 7
-    
-    if d1 == 0:
-        m1 += 1
-        d1 = months[m1]
+        d1 -= 1
+        day -= 1
+
+        if day == 0:
+            day = 7
+        
+        if d1 == 0:
+            m1 += 1
+            d1 = months[m1]
+
+# 1이 2보다 이전
+else:
+    while True:
+        if m1 == m2 and d1 == d2:
+            break
+        d1 += 1
+        day += 1
+
+        if day == 8:
+            day = 1
+        
+        if d1 == months[m1]:
+            m1 += 1
+            d1 = 1
 
 print(days[day])
