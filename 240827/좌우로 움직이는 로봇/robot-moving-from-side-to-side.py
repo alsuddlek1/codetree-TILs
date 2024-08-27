b4acc9516cc44e5e823fb2285e262104de1b4243
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-08-27 20:30:12 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240827/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240827/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,49 +1,40 @@
 MAX_T = 50000
-N, M = map(int, input().split())
-arr_A, arr_B = [0] * MAX_T, [0] * MAX_T
-time_A, time_B = 0, 0
-idx_A, idx_B = 0, 0
 
-# A 로봇
-for i in range(N):
-    t, d = input().split()
-    t = int(t)
-    for j in range(t):
-        if d == "R":
-            arr_A[idx_A] = time_A
-            time_A = time_A + 1
-            idx_A += 1
-        else:
-            arr_A[idx_A] = time_A
-            time_A = time_A - 1
-            idx_A += 1
-    # for k in range(idx_A, MAX_T):
-    #     arr_A[k] = time_A
-    length = len(arr_A[idx_A:])
-    arr_A[idx_A:] = [time_A] * length
+def process_commands(N, commands, arr):
+    time = 0
+    idx = 0
+    
+    for i in range(N):
+        t, d = commands[i]
+        t = int(t)
+        for j in range(t):
+            if d == "R":
+                arr[idx] = time
+                time += 1
+            else:
+                arr[idx] = time
+                time -= 1
+            idx += 1
+        for k in range(idx, MAX_T):
+            arr[k] = time
 
-# B 로봇
-for i in range(M):
-    t, d = input().split()
-    t = int(t)
-    for j in range(t):
-        if d == "R":
-            arr_B[idx_B] = time_B
-            time_B = time_B + 1
-            idx_B += 1
-        else:
-            arr_B[idx_B] = time_B
-            time_B = time_B - 1
-            idx_B += 1
-    # for k in range(idx_B, MAX_T):
-    #     arr_B[k] = time_B
-    length = len(arr_B[idx_B:])
-    arr_B[idx_B:] = [time_B] * length
+def main():
+    N, M = map(int, input().split())
+    arr_A, arr_B = [0] * MAX_T, [0] * MAX_T
+    
+    commands_A = [input().split() for _ in range(N)]
+    commands_B = [input().split() for _ in range(M)]
+    
+    process_commands(N, commands_A, arr_A)
+    process_commands(M, commands_B, arr_B)
+    
+    # 판별
+    cnt = 0
+    for i in range(1, MAX_T):
+        if arr_A[i] == arr_B[i] and arr_A[i-1] != arr_B[i-1]:
+            cnt += 1
+    
+    print(cnt)
 
-# 판별
-cnt = 0
-for i in range(1, len(arr_A)):
-    if arr_A[i] == arr_B[i] and arr_A[i-1] != arr_B[i-1]:
-        cnt += 1
-
-print(cnt)
+if __name__ == "__main__":
+    main()
