e0660b3e2a016fa9bd3ce6b41b5da57b33cc1884
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-08-28 10:17:55 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240828/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240828/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -10,8 +10,6 @@ for i in range(N):
     for j in range(t):
         arr_A[time_A] = arr_A[time_A-1] + (1 if d=="R" else -1)
         time_A += 1
-    for k in range(time_A, MAX_T):
-        arr_A[k] = arr_A[time_A-1]
 
 # b robot
 for i in range(M):
@@ -20,8 +18,14 @@ for i in range(M):
     for j in range(t):
         arr_B[time_B] = arr_B[time_B-1] + (1 if d=="R" else -1)
         time_B += 1
-    for k in range(time_B, MAX_T):
-        arr_B[k] = arr_B[time_B-1]
+
+# 마지막 시간부터 배열 마지막까지 같은 수 채우기
+if time_A < time_B:
+    for i in range(time_A, time_B):
+        arr_A[i] = arr_A[i-1]
+elif time_B > time_A:
+    for i in range(time_B, time_A):
+        arr_B[i] = arr_B[i-1]
 
 # 배열비교
 result = 0
