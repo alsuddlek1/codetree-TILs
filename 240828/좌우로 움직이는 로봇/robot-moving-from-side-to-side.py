5e29dd62ea38fa1223209bdef57b5b3ac1700ab1
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-08-28 10:19:17 +0900

    Title: 좌우로 움직이는 로봇, Time: 260ms, Memory: 41MB, Status: Passed - CodeTree

--- 240828/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240828/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -1,37 +1,37 @@
-MAX_T = 50000
-N, M = map(int, input().split())
-arr_A, arr_B = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
-time_A, time_B = 1, 1
+MAX_T = 1000000
 
-# a robot
-for i in range(N):
-    t, d = input().split()
-    t = int(t)
-    for j in range(t):
-        arr_A[time_A] = arr_A[time_A-1] + (1 if d=="R" else -1)
-        time_A += 1
+# 변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
 
-# b robot
-for i in range(M):
-    t, d = input().split()
-    t = int(t)
-    for j in range(t):
-        arr_B[time_B] = arr_B[time_B-1] + (1 if d=="R" else -1)
-        time_B += 1
+# A가 매 초마다 서있는 위치를 기록
+time_a = 1
+for _ in range(n):
+    t, d = tuple(input().split())
+    for _ in range(int(t)):
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+        time_a += 1
 
-# 마지막 시간부터 배열 마지막까지 같은 수 채우기
-if time_A < time_B:
-    for i in range(time_A, time_B):
-        arr_A[i] = arr_A[i-1]
-elif time_B > time_A:
-    for i in range(time_B, time_A):
-        arr_B[i] = arr_B[i-1]
+# B가 매 초마다 서있는 위치를 기록
+time_b = 1
+for _ in range(m):
+    t, d = tuple(input().split())
+    for _ in range(int(t)):
+        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
+        time_b += 1
 
-# 배열비교
-result = 0
-time_max = max(time_A, time_B)
-for i in range(time_max):
-    if arr_A[i] == arr_B[i] and arr_A[i-1] != arr_B[i-1]:
-        result += 1
+if time_a < time_b:
+	for i in range(time_a, time_b):
+		pos_a[i] = pos_a[i - 1]
+elif time_a > time_b:
+	for i in range(time_b, time_a):
+		pos_b[i] = pos_b[i - 1]
 
-print(result)
+# 새롭게 만나는 횟수를 구합니다.
+cnt = 0
+time_max = max(time_a, time_b)
+for i in range(1, time_max):
+    if pos_a[i] == pos_b[i] and pos_a[i - 1] != pos_b[i - 1]:
+        cnt += 1
+        
+print(cnt)
