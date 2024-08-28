8041b975357d43e2eccde02f35f1bd53a712fdc5
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-08-28 11:33:32 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240828/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240828/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,8 +1,13 @@
 N, K, P, T = map(int, input().split())
 data = [[0]*3 for _ in range(T)]
 
+# 감염 경험 체크 리스트
 result = [0] * (N+1)
-result[K] = 1
+result[P] = 1
+
+# 감염 가능 횟수 리스트
+cnt = [0] * (N+1)
+cnt[P] = K
 
 # data 만들기
 for i in range(T):
@@ -14,11 +19,32 @@ for i in range(T):
 # data 정렬
 data.sort(key = lambda x : (x[0], x[1]))
 
-for i in range(K):
-    if result[data[i][1]] == 1:
-        result[data[i][2]] = 1
-    elif result[data[i][2]] == 1:
-        result[data[i][1]] = 1
+# ----------------
+
+for i in range(T):
+    # x가 점염 가능성 有
+    if cnt[data[i][1]] >= 1:
+        # y 도 감염
+        if cnt[data[i][2]] >= 1:
+            cnt[data[i][2]] -= 1
+        # y는 첫 감염
+        else:
+            result[data[i][2]] = 1
+            cnt[data[i][2]] = K
+        cnt[data[i][1]] -= 1
+
+    
+    # y가 점염 가능성
+    elif cnt[data[i][2]] >= 1:
+        # x 도 감염
+        if cnt[data[i][1]] >= 1:
+            cnt[data[i][1]] -= 1
+        # x는 첫 감염
+        else:
+            result[data[i][1]] = 1
+            cnt[data[i][1]] = K
+        cnt[data[i][2]] -= 1
+
 
 result = ''.join(map(str, result[1:]))
 print(result)
