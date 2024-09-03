b481b9411d2651d642292789b002780b0b55820e
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-03 21:28:57 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 160ms, Memory: 73MB, Status: Passed - CodeTree

--- 240903/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240903/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -1,12 +1,12 @@
 N, M = map(int, input().split())
 
-snail = [[0]*N for _ in range(M)]
+snail = [[0]*M for _ in range(N)]
 
 dx = [0, 1, 0, -1]  # 우, 하, 좌, 상
 dy = [1, 0, -1, 0]
 
 def in_range(x, y):
-    return 0 <= x < M and 0 <= y < N
+    return 0 <= x < N and 0 <= y < M
 
 x, y = 0, 0  # 달팽이 시작 위치
 cnt = 1
@@ -21,9 +21,11 @@ while cnt < N*M:
         snail[nx][ny] = cnt
         x, y = nx, ny
     else:
-        dir = (dir + 1) % 4
+        dir += 1
+        if dir >= 4:
+            dir = 0
 
-for i in range(M):
-    for j in range(N):
+for i in range(N):
+    for j in range(M):
         print(snail[i][j], end=" ")
     print()
