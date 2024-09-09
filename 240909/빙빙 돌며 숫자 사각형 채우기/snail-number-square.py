5cdd5e6178958871a611b6f1166d8508a7ad1eb6
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-09 23:09:30 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240909/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
+++ 240909/빙빙 돌며 숫자 사각형 채우기/snail-number-square.py
@@ -13,30 +13,26 @@ cnt = 1
 snail[x][y] = cnt
 dir = 0
 
-while cnt < N*M:
-    nx = x + dx[dir]
-    ny = y + dy[dir]
-    if in_range(nx, ny) and snail[nx][ny] == 0:
-        cnt += 1
-        snail[nx][ny] = cnt
-        x, y = nx, ny
-    else:
-        dir += 1
-        if dir >= 4:
-            dir = 0
+# while cnt < N*M:
+#     nx = x + dx[dir]
+#     ny = y + dy[dir]
+#     if in_range(nx, ny) and snail[nx][ny] == 0:
+#         cnt += 1
+#         snail[nx][ny] = cnt
+#         x, y = nx, ny
+#     else:
+#         dir += 1
+#         if dir >= 4:
+#             dir = 0
 
-# for i in range(2, N * M + 1):
-#     # 현재 방향 dir를 기준으로 그 다음 위치 값을 계산합니다.
-#     nx, ny = x + dx[dir], y + dy[dir]
+for i in range(2, N * M + 1):
+    nx, ny = x + dx[dir], y + dy[dir]
     
-#     # 더 이상 나아갈 수 없다면
-#     # 시계방향으로 90'를 회전합니다.
-#     if not in_range(nx, ny) or snail[nx][ny] != 0:
-#         dir = (dir + 1) % 4
+    if not in_range(nx, ny) or snail[nx][ny] != 0:
+        dir = (dir + 1) % 4
 
-#     # 그 다음 위치로 이동한 다음 배열에 올바른 값을 채워넣습니다.
-#     x, y = x + dx[dir], y + dy[dir]
-#     snail[x][y] = i
+    x, y = x + dx[dir], y + dy[dir]
+    snail[x][y] = i
 
 for i in range(N):
     for j in range(M):
