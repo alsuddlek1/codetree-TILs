e2b4106cfa8b46a814a459993147882212f019b2
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-10 19:52:25 +0900

    Title: 격자 위의 편안한 상태, Time: 178ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240910/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,31 @@
+N, M = map(int, input().split())
+matrix = list([0]*(N+1) for _ in range(N+1))
+res = 0
+
+## 판단 함수
+def judge(x, y):
+    global res
+    global matrix
+    dx = [0, 0, -1, 1] # 좌우상하
+    dy = [-1, 1, 0, 0]
+    cnt = 0
+    for dir in range(4):
+        nx, ny = x+dx[dir], y+dy[dir]
+        if 0<=nx<N+1 and 0<=ny<N+1 and matrix[nx][ny] == 1:
+            cnt += 1
+    if cnt >= 3:
+        return True
+    else:
+        return False
+
+
+for i in range(M):
+    x, y = map(int, input().split())
+
+    # 색칠하기
+    matrix[x][y] = 1
+    done = judge(x,y)
+    if done:
+        print(1)
+    else:
+        print(0)
