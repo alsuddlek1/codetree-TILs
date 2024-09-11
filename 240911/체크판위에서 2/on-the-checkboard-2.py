d93467501d803402cb7bb6103bf597b780691d04
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-11 14:50:41 +0900

    Title: 체크판위에서 2, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240911/체크판위에서 2/on-the-checkboard-2.py
@@ -0,0 +1,12 @@
+R, C = map(int, input().split())
+arr = list(list(input().split()) for _ in range(R))
+
+cnt = 0
+for i in range(1, R):
+    for j in range(1, C):
+        for k in range(i+1, R-1):
+            for l in range(j+1, C-1):
+                if arr[0][0] != arr[i][j] and arr[i][j] != arr[k][l] and arr[k][l] != arr[R-1][C-1]:
+                    cnt +=1
+
+print(cnt)
