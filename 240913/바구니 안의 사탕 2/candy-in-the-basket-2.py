0d7467aaba47fc1ea43da624a1bef799be9ed5a7
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-13 15:09:35 +0900

    Title: 바구니 안의 사탕 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240913/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240913/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,15 +1,13 @@
 n, k = map(int, input().split())
 data = [0]*100
-max_value = 0
 
-for _ in range(4):
+for _ in range(n):
     c, l = map(int, input().split())
-    max_value = max(max_value, l)
     data[l] += c
 
 result = 0
 
-for i in range(k+1, max_value-k+1):
+for i in range(k+1, 100-k):
     cnt = data[i]
     for j in range(1, k+1):
         cnt += data[i+j]
