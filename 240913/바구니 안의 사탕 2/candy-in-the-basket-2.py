f75f6f628d31a7c5ab01fbf3fa971f2256591ed8
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-13 15:21:07 +0900

    Title: 바구니 안의 사탕 2, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240913/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240913/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,5 +1,5 @@
 n, k = map(int, input().split())
-data = [0]*100
+data = [0]*101
 
 for _ in range(n):
     c, l = map(int, input().split())
@@ -7,11 +7,15 @@ for _ in range(n):
 
 result = 0
 
-for i in range(k+1, 100-k):
-    cnt = data[i]
-    for j in range(1, k+1):
-        cnt += data[i+j]
-        cnt += data[i-j]
-    result = max(result, cnt)
+if k < 50:
+    print(1234343)
+    for i in range(k+1, 100-k):
+        cnt = data[i]
+        for j in range(1, k+1):
+            cnt += data[i+j]
+            cnt += data[i-j]
+        result = max(result, cnt)
+else:
+    result = sum(data)
 
 print(result)
