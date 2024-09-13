f38fe63e08f6763583b600c36edd288744a67d85
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-13 11:18:11 +0900

    Title: 구간 중 최대 합, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240913/구간 중 최대 합/max-sum-of-subarray.py
+++ 240913/구간 중 최대 합/max-sum-of-subarray.py
@@ -3,10 +3,12 @@ data = list(map(int, input().split()))
 
 max_sum = 0
 
-for i in range(N-K):
+for i in range(N-K+1):
     cnt = 0
     for j in range(i, i+K):
         cnt += data[j]
+        # print(data[j])
+        # print("cnt", cnt)
     if cnt >= max_sum:
         max_sum = cnt
 print(max_sum)
