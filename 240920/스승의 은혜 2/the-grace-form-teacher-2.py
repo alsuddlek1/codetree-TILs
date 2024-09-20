83d459dc1706a333b5cfb5a2101de06780fc1ac2
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-09-20 16:01:09 +0900

    Title: 스승의 은혜 2, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240920/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 240920/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -5,13 +5,18 @@ students = 0
 ## 한정된 양으로 가장 많은 경우를 찾는 문제 -> 정렬
 data = []
 for _ in range(N):
-    data.append(int(input()))
+    cnt = int(input())
+    data.append(cnt)
 
 data.sort()
+data[-1] = data[-1]//2
+data.sort()
+# print(data)
 
 for i in range(N):
     B -= data[i]
     students += 1
+    # print(B, students)
     if B <= 0:
         break
 
