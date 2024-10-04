f76cb999d5659ca489aa4179ed52ab4188eaf1ca
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-10-04 15:02:16 +0900

    Title: 선택 정렬 구현, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241004/선택 정렬 구현/implement-selection-sort.py
@@ -0,0 +1,16 @@
+N = int(input())
+data = list(map(int, input().split()))
+
+def selection_sort(N, arr):
+    for i in range(N-1):
+        minimum = i
+        for j in range(i+1, N):
+            if arr[j] < arr[minimum]:
+                minimum = j
+            tmp = arr[i]
+            arr[i] = arr[minimum]
+            arr[minimum] = tmp
+            print(arr)
+    return arr
+
+print(selection_sort(N, data))
