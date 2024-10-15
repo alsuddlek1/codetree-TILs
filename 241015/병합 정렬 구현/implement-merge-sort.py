39231e2ebbcfb377f6ee45d13368a8324518d7d5
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-10-15 16:23:26 +0900

    Title: 병합 정렬 구현, Time: 370ms, Memory: 32MB, Status: Passed - CodeTree

--- 241015/병합 정렬 구현/implement-merge-sort.py
+++ 241015/병합 정렬 구현/implement-merge-sort.py
@@ -1,6 +1,44 @@
 n = int(input())
-data = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+merged_arr = [0] * n
 
-data.sort()
-for i in range(n):
-    print(data[i], end=" ")
+def merge(low, mid, high):
+    i, j = low, mid + 1
+    
+    k = low 
+    while i <= mid and j <= high:
+        if arr[i]<= arr[j]:
+            merged_arr[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            merged_arr[k] = arr[j]
+            k += 1
+            j += 1
+
+    while i <= mid:
+        merged_arr[k] = arr[i]
+        k += 1
+        i += 1
+    
+    while j <= high:
+        merged_arr[k] = arr[j]
+        k += 1
+        j += 1
+    
+    for k in range(low, high + 1):
+        arr[k] = merged_arr[k]
+
+
+def merge_sort(low, high):
+    if low < high:                    
+        mid = (low + high) // 2
+        merge_sort(low, mid)
+        merge_sort(mid + 1, high)
+        merge(low, mid, high)
+
+
+merge_sort(0, n - 1)
+
+for elem in arr:
+    print(elem, end=" ")
