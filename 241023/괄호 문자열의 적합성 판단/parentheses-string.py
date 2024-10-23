a400af50f66a9a89b02a93841c4cb2784d599896
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-10-23 15:26:46 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 142ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241023/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 241023/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -1,15 +1,19 @@
 data = input()
 stack = []
+result = "()"
+res = ""
 
 for i in data:
-    if len(stack) == 0:
+    if i == "(" and len(stack) == 0:
         stack.append(i)
     else:
-        top = stack[-1]
-        if i != top:
-            stack.pop()
-        else:
-            stack.append(i)
+        if len(stack) > 0:
+            top = stack[-1]
+            if i != top:
+                stack.pop()
+            else:
+                stack.append(i)
+   
 
 if len(stack) == 0:
     print("Yes")
