7265a3b57451a4937cf3979fe0038c0694d8a62d
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-11-12 15:02:43 +0900

    Title: 행복한 수열의 개수, Time: 58ms, Memory: 16MB, Status: Passed - CodeTree

--- 241112/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241112/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,40 +1,40 @@
 ## n*n
 ## 행복한 수열 : 연속하여 m개 이상의 동일한 원소가 나오는 순간이 존재하는 수열
 
-# 1. 변수 선언
-n, m = map(int, input().split())
+# 각 열/행 -> def 비교
+
+# 1. 변수선언
+n,m = map(int, input().split())
 data = [list(map(int, input().split())) for _ in range(n)]
 
 answer = 0
 
-# 2. 수열 찾기
-# 2-1. 행 수열
-for i in range(n):
+# 2. 비교 함수
+def check_happy(arr, m):
     cnt = 1
-    for j in range(n-1):
-        # print(data[i][j])
-        if data[i][j] == data[i][j+1]:
+    # 2-1. 비교할 리스트가 맞으면 ture 아니면 false 반환
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i-1]:
             cnt += 1
-            if cnt == m:
-                answer += 1
+            if cnt >= m:
+                return True
         else:
             cnt = 1
+    if m == 1:
+        return True
+    return False
 
-
-# print()
-# 2-2. 열 수열
+# 3. 행 비교
 for i in range(n):
-    cnt = 1
-    for j in range(n-1):
-        # print(data[j][i])
-        if data[j][i] == data[j+1][i]:
-            cnt += 1
-            if cnt == m:
-                answer += 1
-        else:
-            cnt = 1
-
-if m == 1:
-    answer = n * 2
+    if check_happy(data[i], m):
+        answer += 1
+    
+# 4. 열 비교
+for i in range(n):
+    row_arr = []
+    for j in range(n):
+        row_arr.append(data[j][i])
+    if check_happy(row_arr, m):
+        answer += 1
 
 print(answer)
