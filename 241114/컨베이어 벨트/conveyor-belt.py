50e2d0b4488b88b459ec478d92c8899818279e3d
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-11-14 14:48:19 +0900

    Title: 컨베이어 벨트, Time: 58ms, Memory: 16MB, Status: Passed - CodeTree

--- 241114/컨베이어 벨트/conveyor-belt.py
+++ 241114/컨베이어 벨트/conveyor-belt.py
@@ -10,12 +10,13 @@ for i in range(2):
 
 # 2. 1차원 배열 밀기
 # 2-1. 오른쪽으로 밀기
-temp = data[-1]
+for k in range(t):
+    temp = data[-1]
 
-for i in range(2*n-2, -1, -1):
-    data[i+1] = data[i]
+    for i in range(2*n-2, -1, -1):
+        data[i+1] = data[i]
 
-data[0] = temp
+    data[0] = temp
 
 # 3. answer
 for i in range(2*n):
