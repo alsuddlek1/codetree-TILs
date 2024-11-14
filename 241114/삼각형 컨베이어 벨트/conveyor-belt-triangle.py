789b5449360e48ca0de03d986cd5b0833ace304f
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-11-14 16:02:57 +0900

    Title: 삼각형 컨베이어 벨트, Time: 59ms, Memory: 17MB, Status: Passed - CodeTree

--- 241114/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
+++ 241114/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -2,21 +2,24 @@
 n, t = map(int, input().split())
 data = []
 
-for i in range(n):
+for i in range(3):
     arr = list(map(int, input().split()))
     for j in range(n):
         data.append(arr[j])
+
+# print(data)
 
 # 2. arr 이동
 for t in range(t):
     temp = data[-1]
 
-    for i in range(n*n-2, -1, -1):
+    for i in range(3*n-2, -1, -1):
         data[i+1] = data[i]
+
     data[0] = temp
 
 # 3. answer
-for i in range(n*n):
+for i in range(3*n):
     print(data[i], end=" ")
     if (i+1) % n == 0:
         print()
