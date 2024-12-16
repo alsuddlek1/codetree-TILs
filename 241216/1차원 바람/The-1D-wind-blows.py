ab1cbc3c4364b933f6f1bb4fafdff2374ea66d88
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-12-16 15:55:48 +0900

    Title: 1차원 바람, Time: 128ms, Memory: 20MB, Status: Passed - CodeTree

--- 241216/1차원 바람/The-1D-wind-blows.py
+++ 241216/1차원 바람/The-1D-wind-blows.py
@@ -1,69 +1,72 @@
-# 특정 행을 왼/오 shift
-# shift 된 위 아래 행은 반대로 shift
+## n*m 행렬
+## 특정 행의 모든 원소들을 왼쪽 혹은 오른쪽으로 민다
+## 위 아래 하나라도 같은 열에 같은 숫자가 적혀있으면 전파가 나아간다
 
-# 2. 특정 행 shift
-def shift(arr, w):
-    if w == "L":
+# 2. 행을 미는 함수
+def shift(arr, d):
+    n = len(arr)
+    if d == "L":
         temp = arr[-1]
-        for i in range(len(arr)-1, 0, -1):
-            arr[i] = arr[i-1]
+        for i in range(n-2, -1, -1):
+            arr[i+1] = arr[i]
         arr[0] = temp
-
-        temp = arr[0]
-        return arr
+    
     else:
         temp = arr[0]
-        for i in range(1, len(arr)):
+        for i in range(1, n):
             arr[i-1] = arr[i]
         arr[-1] = temp
-        return arr
-
-
-# 3. 같은 수 판단
-def same_num_judge(arr1, arr2):
-    for i in range(m):
-        if arr1[i] == arr2[i]:
-            return True
-
-# 5. 방향 뒤짚기
-def wind(w):
-    if w == "L":
-        w = "R"
-    else:
-        w = "L"
-    return w
-
-
-# 1. 변수 선언
-n, m, q = map(int, input().split())
-data = [list(map(int, input().split())) for _ in range(n)]
-r, d = input().split()
-r = int(r) - 1
+    return arr
 
 # 4. 전파
-for Q in range(q):
-    shift(data[r], d)
-    d_2 = wind(d)
 
-    # 4-1. 0 ~ r-1
-    for i in range(r-1, -1, -1):
-        if same_num_judge(data[i+1], data[i]):
-            shift(data[i], d_2)
-            d_2 = wind(d_2)
+
+
+# 1. 변수선언
+n, m, q = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+# 3. data
+for i in range(q):
+    r, d = input().split()
+    r = int(r) - 1
+
+    d_2 = d
+
+    data[r] = shift(data[r], d)
+
+    # 3-1. 0~r-1
+    for j in range(r-1, -1, -1):        
+        if d_2 == "L":
+            d_2 = "R"
+        else:
+            d_2 = "L"
+
+        for k in range(m):
+            if data[j+1][k] == data[j][k]:
+                data[j] = shift(data[j], d_2)
+                break
         else:
             break
 
     d_2 = d
-    d_2 = wind(d)
 
-    # 4-2. r+1 ~ n
-    for i in range(r+1, n):
-        if same_num_judge(data[i-1], data[i]):
-            shift(data[i], d_2)
-            d_2 = wind(d_2)
+    # 3-1. 0~r-1
+    for j in range(r+1, n, 1):
+        if d_2 == "L":
+            d_2 = "R"
+        else:
+            d_2 = "L"
+
+        for k in range(m):
+            if data[j-1][k] == data[j][k]:
+                data[j] = shift(data[j], d_2)
+                break
         else:
             break
 
+
+# 5. answer
 for i in range(n):
     for j in range(m):
         print(data[i][j], end=" ")
