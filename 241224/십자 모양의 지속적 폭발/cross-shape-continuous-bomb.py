e07653368efee5b6123dc55bf67428204028e43a
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-12-24 16:43:32 +0900

    Title: 십자 모양의 지속적 폭발, Time: 121ms, Memory: 20MB, Status: Wrong Answer - CodeTree

--- 241224/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
+++ 241224/십자 모양의 지속적 폭발/cross-shape-continuous-bomb.py
@@ -4,11 +4,8 @@ def in_range(x,y):
 
 # 2. 폭발 함수
 def bomb(size, x, y):
-    print(size)
-
     dxs = [0, -1, 1, 0, 0] # 상, 하, 좌, 우
     dys = [0, 0, 0, -1, 1]
-    print("x,y",x,y)
     if size == 1:
         grid[x][y] = 0
     else:
@@ -17,7 +14,6 @@ def bomb(size, x, y):
                 nx = x + dx*s
                 ny = y + dy*s
                 if in_range(nx, ny) and grid[nx][ny] != 0:
-                    print(nx, ny)
                     grid[nx][ny] = 0
                     ## 중력
 
@@ -28,14 +24,12 @@ def gravity():
         for j in range(n):
             if grid[j][i] != 0:
                 arr.append(grid[j][i])
-        print(arr)
 
         new_arr = [0 for _ in range(n)]
-        for i in range(n-len(arr), n):
-            new_arr[i] = arr[i-(n-len(arr))]
-        print(new_arr)
-
-
+        for k in range(n-len(arr), n):
+            new_arr[k] = arr[k-(n-len(arr))]
+        for l in range(n):
+            next_grid[l][i] = new_arr[l]
 
 # 1. 변수 선언
 n, m = map(int, input().split())
@@ -47,6 +41,10 @@ for c in range(m):
         if grid[i][c] != 0:
             size = grid[i][c]
             bomb(size, i, c)
-            print(grid)
             gravity()
             break
+
+for i in range(n):
+    for j in range(n):
+        print(next_grid[i][j], end=" ")
+    print()
