0a131509c2721ade70701bca56431e389582d1dd
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-12-26 21:41:19 +0900

    Title: 2차원 폭발 게임, Time: 232ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241226/2차원 폭발 게임/The-2D-bomb-game.py
+++ 241226/2차원 폭발 게임/The-2D-bomb-game.py
@@ -9,7 +9,6 @@ def rotate():
 
 
 
-
 # 3. 중력
 def gravity():
     for i in range(n):
@@ -24,9 +23,9 @@ def gravity():
             data[l][i] = next_arr[l]
             
 
-
 # 2. m개 이상 폭발
 def bomb(m):
+    value = False
     for i in range(n):
         arr = []
         for j in range(n):
@@ -35,15 +34,22 @@ def bomb(m):
         for k in range(n-m+1):
             bomb_index = []
             for l in range(k, n):
-                if arr[k] == arr[l]:
+                if arr[k] == arr[l] and arr[k] != 0:
                     bomb_index.append(l)
                 else:
                     break
+            
             if len(bomb_index) >= m:
+                value = True
                 for p in bomb_index:
                     arr[p] = 0
+                
         for x in range(n):
             data[x][i] = arr[x]
+
+    return value
+
+
 
 
 # 1. 변수선언
@@ -52,8 +58,18 @@ data = [list(map(int, input().split())) for _ in range(n)]
 
 # 5. k번 반복
 for i in range(k):
-    bomb(m)
-    gravity()
+    ## bomb과 gravity -> 더이상 터질 게 없을 때 까지 반복
+    # print(bomb(m))
+    # gravity()
+    # print(data)
+    while True:
+        value = bomb(m)
+        gravity()
+        
+        if value == False:
+            break
+
+    # 더이상 터질게없다면 회전
     data = rotate()
     gravity()
 
