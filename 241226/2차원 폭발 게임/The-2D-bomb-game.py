0dc2ea0b5df92232f87c643f522cbec2b663237d
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-12-26 11:01:41 +0900

    Title: 2차원 폭발 게임, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241226/2차원 폭발 게임/The-2D-bomb-game.py
@@ -0,0 +1,17 @@
+# 4. 회전
+
+# 3. 중력
+
+# 2. m개 이상 폭발
+def bomb(m):
+    print(m)
+    print(data)
+
+
+# 1. 변수선언
+n, m, k = map(int, input().split())
+data = [list(map(int, input().split())) for _ in range(n)]
+
+# 5. k번 반복
+for i in range(k):
+    bomb(m)
