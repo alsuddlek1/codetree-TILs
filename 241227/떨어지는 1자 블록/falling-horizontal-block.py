06a2d56c78b108b5a75d16a8a3e77f2c00c3de77
민영 <122499580+alsuddlek1@users.noreply.github.com>
2024-12-27 13:48:12 +0900

    Title: 떨어지는 1자 블록, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 241227/떨어지는 1자 블록/falling-horizontal-block.py
+++ 241227/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,34 +1,53 @@
-# 1. 변수 선언
-n, m, k = map(int, input().split())
-k = k-1
-grid = [list(map(int, input().split())) for _ in range(n)]
+# 변수 선언 및 입력: 
 
-# 1-1. 0번째 열부터 시작, 0번째 행부터 아래로 내려가기 
-x, y = 0, k
+n, m, k = tuple(map(int, input().split()))
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
 
+# 해당 row에 [col_s, col_e] 열에
+# 전부 블럭이 없는지를 확인합니다.
+# def all_blank(row, col_s, col_e):
+#     return all([
+#         not grid[row][col]
+#         for col in range(col_s, col_e + 1)
+#     ])
 
-# 2-1. 범위 설정
-def in_range(x, y):
-    return 0 <= x < n and 0 <= y < n
-
-# 2. 맞닿는 지 확인
-def check(r, k):
-    for i in range(m):
-        # 행은 아래로, 열은 m 길이 만큼
-        if grid[r+1][k+i] == 1:
+def all_blank(row, col_s, col_e):
+    results = []
+    for col in range(col_s, col_e+1):
+        if not grid[row][col]:
+            results.append(True)
+        else:
+            results.append(False)
+    
+    for result in results:
+        if not result:
             return False
+    
     return True
 
-while True:
-    if in_range(x, y) and check(x, y):
-        x += 1
-    else:
-        for i in range(m):
-            grid[x][y+i] = 1
-        
-        break
 
-# 3. 출력
+# 최종적으로 도달하게 될 위치는
+# 그 다음 위치에 최초로 블럭이 존재하는 순간임을 이용합니다.
+def get_target_row():
+    for row in range(n - 1):
+        if not all_blank(row + 1, k, k + m - 1):
+            return row
+
+    return n - 1
+        
+
+k -= 1
+
+# 최종적으로 멈추게 될 위치를 구합니다.
+target_row = get_target_row()
+
+# 최종 위치에 전부 블럭을 표시합니다.
+for col in range(k, k + m):
+    grid[target_row][col] = 1
+
 for i in range(n):
     for j in range(n):
         print(grid[i][j], end=" ")
