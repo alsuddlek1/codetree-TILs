aa8e1cb81eac4183e5d21119913678670b1830cd
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-08 15:28:38 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 250108/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 250108/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,9 +1,41 @@
 n, r, c = map(int, input().split())
-a = [[0] * (n + 1) for _ in range(n + 1)]
+r,c = r-1, c-1
+data = [list(map(int, input().split())) for _ in range(n)]
+max_cnt = data[r][c]
+answer = [data[r][c]]
+x, y = r, c
 
-for i in range(1, n + 1):
-    row = list(map(int, input().split()))
-    for j in range(1, n + 1):
-        a[i][j] = row[j - 1]
+def in_range(x,y):
+    return 0 <= x < n and 0 <= y < n
 
-# Write your code here!
+def move(x,y, max_cnt):
+    value = False
+
+    dxs = [-1, 1, 0, 0] # 상하좌우
+    dys = [0, 0, -1, 1]
+
+    max_pos = (x, y)
+    for dx, dy in zip(dxs, dys):
+        nx = x + dx
+        ny = y + dy
+        if in_range(nx, ny) and data[nx][ny] > max_cnt:
+            max_cnt = data[nx][ny]
+            max_pos = (nx, ny)
+            value = True
+            break
+    x, y = max_pos
+
+    return max_cnt, max_pos, value
+
+while True:
+    max_cnt, max_pos, value = move(x, y, max_cnt)
+    
+    if value == False:
+        break
+
+    x, y = max_pos[0], max_pos[1]
+    answer.append(data[x][y])
+    
+
+for i in answer:
+    print(i, end=" ")
