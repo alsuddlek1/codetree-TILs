854f8d4bf83d0a293bfd4d67b9fe60c19453fdbf
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-08 09:46:52 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250108/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -0,0 +1,9 @@
+n, r, c = map(int, input().split())
+a = [[0] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    row = list(map(int, input().split()))
+    for j in range(1, n + 1):
+        a[i][j] = row[j - 1]
+
+# Write your code here!
