52c95b3c4f635cf0a36cff5364c056cc0c14e35b
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-14 16:06:25 +0900

    Title: 알파벳과 사칙연산, Time: 98ms, Memory: 19MB, Status: Passed - CodeTree

--- 250114/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 250114/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,45 +1,58 @@
-input_formula = input()
-formula = []
-calculation = ["+", "-", "*"]
-num_cnt = 0
+input_expression = input()
+alphabet_list = []
+calculation_list = []
 answer = -1000000
 
-for i in input_formula:
+calculation = ["+", "-", "*"]
+for i in input_expression:
     if i not in calculation:
-        num_cnt += 1
+        alphabet_list.append(i)
     else:
-        formula.append(i)
+        calculation_list.append(i)
 
-## 2. num 숫자와 formula 연산 조합해서 가장 큰 수 출력
-# 2-1. 예) num[0] formula[1] num[1] formula[2] num[2]
+# 2. calculate
 def calculate():
-    cnt = num[0]
-    for i in range(1, num_cnt):
-        if formula[i-1] == "+":
-            cnt = cnt + num[i]
-        elif formula[i-1] == "-":
-            cnt = cnt - num[i]
-        elif formula[i-1] == "*":
-            cnt = cnt * num[i]
+    # 2-1. 알파벳 별 index 지정
+    num_index = []
+    for i in alphabet_list:
+        if i == "a":
+            num_index.append(0)
+        elif i == "b":
+            num_index.append(1)
+        elif i == "c":
+            num_index.append(2)
+        elif i == "d":
+            num_index.append(3)
+        elif i == "e":
+            num_index.append(4)
+        elif i == "f":
+            num_index.append(5)
+    
+    # 2-1. num_list의 num_index에 맞추어 계산
+    cnt = num_list[num_index[0]]
+    for i in range(len(num_index)-1):
+        if calculation_list[i] == "+":
+            cnt += num_list[num_index[i+1]]
+        elif calculation_list[i] == "-":
+            cnt -= num_list[num_index[i+1]]
+        elif calculation_list[i] == "*":
+            cnt *= num_list[num_index[i+1]]
 
     return cnt
-    
 
-## 1. 식의 변수 수에 맞는 num_cnt 자리 수 백트래킹
-num = []
+# 1. a~f 수 할당
+num_list = []
 def back(curr_num):
     global answer
-    if curr_num == num_cnt+1:
+    if curr_num == 6:
         answer = max(answer, calculate())
         return
-    
+
     for i in range(1, 5):
-        num.append(i)
+        num_list.append(i)
         back(curr_num + 1)
-        num.pop()
-    
+        num_list.pop()
     return
 
-back(1)
-
+back(0)
 print(answer)
