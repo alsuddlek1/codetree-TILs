ab7f29aae9b0157335d5a25513a4f422695beaa3
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-14 16:11:50 +0900

    Title: 알파벳과 사칙연산, Time: 96ms, Memory: 18MB, Status: Passed - CodeTree

--- 250114/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 250114/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -10,23 +10,12 @@ for i in input_expression:
     else:
         calculation_list.append(i)
 
+alphabet_to_index = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5}
+
 # 2. calculate
 def calculate():
     # 2-1. 알파벳 별 index 지정
-    num_index = []
-    for i in alphabet_list:
-        if i == "a":
-            num_index.append(0)
-        elif i == "b":
-            num_index.append(1)
-        elif i == "c":
-            num_index.append(2)
-        elif i == "d":
-            num_index.append(3)
-        elif i == "e":
-            num_index.append(4)
-        elif i == "f":
-            num_index.append(5)
+    num_index = [alphabet_to_index[char] for char in alphabet_list]
     
     # 2-1. num_list의 num_index에 맞추어 계산
     cnt = num_list[num_index[0]]
