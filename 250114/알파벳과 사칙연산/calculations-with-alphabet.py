26291903a3a7a6e13c31b541c28520595de9aa63
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-14 14:46:08 +0900

    Title: 알파벳과 사칙연산, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250114/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,45 @@
+input_formula = input()
+formula = []
+calculation = ["+", "-", "*"]
+num_cnt = 0
+answer = -1000000
+
+for i in input_formula:
+    if i not in calculation:
+        num_cnt += 1
+    else:
+        formula.append(i)
+
+## 2. num 숫자와 formula 연산 조합해서 가장 큰 수 출력
+# 2-1. 예) num[0] formula[1] num[1] formula[2] num[2]
+def calculate():
+    cnt = num[0]
+    for i in range(1, num_cnt):
+        if formula[i-1] == "+":
+            cnt = cnt + num[i]
+        elif formula[i-1] == "-":
+            cnt = cnt - num[i]
+        elif formula[i-1] == "*":
+            cnt = cnt * num[i]
+    print(num, cnt)
+    return cnt
+    
+
+## 1. 식의 변수 수에 맞는 num_cnt 자리 수 백트래킹
+num = []
+def back(curr_num):
+    global answer
+    if curr_num == num_cnt+1:
+        answer = max(answer, calculate())
+        return
+    
+    for i in range(1, 5):
+        num.append(i)
+        back(curr_num + 1)
+        num.pop()
+    
+    return
+
+back(1)
+
+print(answer)
