4c71f8a58c075185d45cc020e8e729360a72f93a
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-16 16:30:33 +0900

    Update find-min-of-possible-series.py

--- 250116/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 250116/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -3,29 +3,25 @@ import sys
 n = int(input())
 found = False
 
-# 1. 모든 수열 만들기 (백트래킹)
+# 1. 모든 수열 만들기 - 백트래킹
 def back(curr_num):
     global found
     if found:
         return
 
-    # 현재 길이까지 좋은 수열인지 판단
     for length in range(1, curr_num // 2 + 1):
         if sequence[-length:] == sequence[-2 * length:-length]:
-            return  # 나쁜 수열이면 가지치기
+            return
 
-    # 길이가 n이면 출력하고 종료
     if curr_num == n + 1:
         found = True
         print("".join(map(str, sequence)))
         return
 
-    # 수열 확장
     for i in range(4, 7):
         sequence.append(i)
         back(curr_num + 1)
         sequence.pop()
 
-# 초기화
 sequence = []
 back(1)
