fd55262d8ca422cf94c0dd8f4127e6466a04ec2e
민영 <122499580+alsuddlek1@users.noreply.github.com>
2025-01-16 16:26:53 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 51ms, Memory: 16MB, Status: Passed - CodeTree

--- 250116/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
+++ 250116/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -3,44 +3,29 @@ import sys
 n = int(input())
 found = False
 
-# 2. 불가능한 수열 판단
-def get_possible():
-    global found
-
-    if found:
-        return
-
-    for i in range(n):
-        slice_seq = sequence[i:]
-        # print(i, slice_seq)
-        for j in range(1, len(slice_seq)//2+1):
-            check = slice_seq[:j]
-            # print("check",j, check)
-            # print("slice", slice_seq[j:j+j])
-            if check == slice_seq[j:j+j]:
-                return
-    
-    found = True
-    for i in sequence:
-        print(i, end="")
-    return
-    
-
-# 1. 모든 수열 만들기
-sequence = []
+# 1. 모든 수열 만들기 (백트래킹)
 def back(curr_num):
     global found
     if found:
         return
-    if curr_num == n+1:
-        get_possible()
+
+    # 현재 길이까지 좋은 수열인지 판단
+    for length in range(1, curr_num // 2 + 1):
+        if sequence[-length:] == sequence[-2 * length:-length]:
+            return  # 나쁜 수열이면 가지치기
+
+    # 길이가 n이면 출력하고 종료
+    if curr_num == n + 1:
+        found = True
+        print("".join(map(str, sequence)))
         return
 
+    # 수열 확장
     for i in range(4, 7):
         sequence.append(i)
         back(curr_num + 1)
         sequence.pop()
 
-    return
-
+# 초기화
+sequence = []
 back(1)
